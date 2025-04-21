5a75177d4f787302fef01f6f259203e3eeaf7eab
iy-ui <ideraoluwa200@gmail.com>
2025-04-21 09:54:47 +0100

    Update Firstt_script.py

--- Stage_0/Firstt_script.py
+++ Stage_0/Firstt_script.py
@@ -1,1 +1,12 @@
 print("Hello, Hackbio")
+Idera_info = {
+    "Name": "Ideraoluwa",
+    "Slack username": "Dee",
+    "Email": "ideraoluwa2002gmail.com",
+    "Hobby": "Swimming",
+    "Country": "Nigeria",
+    "Discipline": "Cell Biology and Genetics",
+    "Preferred programming language": "Python"
+}
+
+print(Idera_info)
